965a2a1467cf5d6c6c64b7161cec2799791a9d84
Ruslanllll <innaryslanllll@gmail.com>
2023-06-12 19:59:49 +0300

    Ы

--- src/main/java/com/rusla/warpradar/warpradar.java
+++ src/main/java/com/rusla/warpradar/warpradar.java
@@ -1,12 +1,12 @@
-package com.example.examplemod;
+package com.rusla.warpradar;
 
 import net.minecraft.init.Blocks;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
-public class ExampleMod
+@Mod(modid = warpradar.MODID, version = warpradar.VERSION)
+public class warpradar
 {
     public static final String MODID = "warpradar";
     public static final String VERSION = "1.0";
