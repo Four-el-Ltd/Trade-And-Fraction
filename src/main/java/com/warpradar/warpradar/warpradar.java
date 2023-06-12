b55af7a561ede835c60ba7b870c9af03ccc04b9b
Ruslanllll <innaryslanllll@gmail.com>
2023-06-12 19:40:52 +0300

    Да

--- src/main/java/com/warpradar/warpradar/warpradar.java
+++ src/main/java/com/warpradar/warpradar/warpradar.java
@@ -8,7 +8,7 @@ import cpw.mods.fml.common.event.FMLInitializationEvent;
 @Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
 public class ExampleMod
 {
-    public static final String MODID = "wradar";
+    public static final String MODID = "warpradar";
     public static final String VERSION = "1.0";
     
     @EventHandler
