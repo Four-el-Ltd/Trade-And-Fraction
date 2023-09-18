16c081cf8ce03f33b90d9c4d5dfad4645328148f
TheVitya2127 <119508042+TheVitya2127@users.noreply.github.com>
2023-09-18 20:00:11 +0400

    Update WarpRadar.java
    Теперь оно требует версию нтма
    
    Signed-off-by: TheVitya2127 <119508042+TheVitya2127@users.noreply.github.com>

--- src/main/java/com/warpradar/main/WarpRadar.java
+++ src/main/java/com/warpradar/main/WarpRadar.java
@@ -5,7 +5,7 @@ import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = warpradar.MODID, version = warpradar.VERSION, dependencies="required-after:hbm")
+@Mod(modid = warpradar.MODID, version = warpradar.VERSION, dependencies="required-after:hbm[1.0.27_X4627]")
 public class warpradar {
     public static final String MODID = "warpradar";
     public static final String VERSION = "1.0";
