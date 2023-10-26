470d8ff6c9b1b32d50c0a69f5022df4cf95bc106
Ruslanllll <innaryslanllll@gmail.com>
2023-10-26 12:01:17 +0300

    concrete_legacy_bricks (copyright) Vitya

--- src/main/java/com/warpradar/registry/ModBlocks.java
+++ src/main/java/com/warpradar/registry/ModBlocks.java
@@ -1,5 +1,6 @@
 package com.warpradar.registry;
 
+import com.hbm.blocks.generic.BlockConcreteColored;
 import com.warpradar.blocks.machines.BlockConverter;
 import com.warpradar.main.WarpRadar;
 
@@ -8,10 +9,20 @@ import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 
 public class ModBlocks {
-  public static Block converter = new BlockConverter(Material.iron).setBlockName("converter")
-      .setBlockTextureName(WarpRadar.MODID + ":converter").setCreativeTab(MainRegistry.BlocksTab);
+  public static Block converter;
+  public static Block concrete_brick_legacy;
+
+  public static void initializeBlocks() {
+    converter = new BlockConverter(Material.iron).setBlockName("converter")
+        .setBlockTextureName(WarpRadar.MODID + ":converter").setCreativeTab(MainRegistry.BlocksTab);
+    concrete_brick_legacy = new BlockConcreteColored(Material.iron).setBlockName("concrete_brick_legacy")
+        .setBlockTextureName(WarpRadar.MODID + ":concrete_brick_legacy").setCreativeTab(MainRegistry.BlocksTab);
+
+  }
 
   public static void registerBlocks() {
+    initializeBlocks();
     GameRegistry.registerBlock(converter, converter.getUnlocalizedName());
+    GameRegistry.registerBlock(concrete_brick_legacy, concrete_brick_legacy.getUnlocalizedName());
   }
 }
