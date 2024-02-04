39f84cf2f975265dc5b8046305ba7b4e79c3c841
Ruslanllll <innaryslanllll@gmail.com>
2024-02-04 16:22:37 +0200

    hotfix 3

--- src/main/java/com/warpradar/main/CommonProxy.java
+++ src/main/java/com/warpradar/main/CommonProxy.java
@@ -43,7 +43,8 @@ public class CommonProxy {
                     .getEntityWorld()
                     .getWorldInfo()
                     .getWorldName());
-        WarpRadar.databaseUrl = "jbdc:sqlite://" + path.toString() + "/warpradar.db";
+        WarpRadar.databaseUrl = "jbdc:sqlite://" + path.toAbsolutePath()
+            .toString() + "/warpradar.db";
 
         ConnectionSource cSource = WarpRadar.getConnectionSource();
         TableUtils.createTable(cSource, Faction.class);
