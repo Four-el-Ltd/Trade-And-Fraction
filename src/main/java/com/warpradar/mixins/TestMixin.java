420b2f401b46788e9b7050087881c7410c708284
Ruslanllll <innaryslanllll@gmail.com>
2024-03-06 00:53:42 +0200

    Teste1

--- src/main/java/com/warpradar/mixins/TestMixin.java
+++ src/main/java/com/warpradar/mixins/TestMixin.java
@@ -1,5 +1,10 @@
 package com.warpradar.mixins;
 
+import net.minecraft.server.MinecraftServer;
+
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(MinecraftServer.class)
 public class TestMixin {
 
 }
---
+++ src/main/java/com/warpradar/mixins/minecraft/EntityCreeperMixin.java
@@ -0,0 +1,25 @@
+package com.warpradar.mixins.minecraft;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import net.minecraft.entity.monster.EntityCreeper;
+import net.minecraft.world.World;
+
+@Mixin(EntityCreeper.class)
+public abstract class EntityCreeperMixin extends EntityCreeper {
+    EntityCreeperMixin(World world) {
+        super(world);
+    }
+
+    @Shadow
+    protected abstract void func_146077_cc();
+
+    @Inject(at = @At("HEAD"), method = "onDeath")
+    protected void onKill(CallbackInfo ci) {
+        this.func_146077_cc();
+    }
+}
