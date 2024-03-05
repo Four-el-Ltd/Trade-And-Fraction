765965c4bd70f98ffbcddb5444d62ec9e570a344
Ruslanllll <innaryslanllll@gmail.com>
2024-03-06 01:07:32 +0200

    spotless

--- src/main/java/com/warpradar/mixins/minecraft/EntityCreeperMixin.java
+++ src/main/java/com/warpradar/mixins/minecraft/EntityCreeperMixin.java
@@ -1,17 +1,18 @@
 package com.warpradar.mixins.minecraft;
 
+import net.minecraft.entity.monster.EntityCreeper;
+import net.minecraft.entity.monster.EntityMob;
+import net.minecraft.world.World;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import net.minecraft.entity.monster.EntityCreeper;
-import net.minecraft.entity.monster.EntityMob;
-import net.minecraft.world.World;
-
 @Mixin(EntityCreeper.class)
 public abstract class EntityCreeperMixin extends EntityMob {
+
     EntityCreeperMixin(World world) {
         super(world);
     }
