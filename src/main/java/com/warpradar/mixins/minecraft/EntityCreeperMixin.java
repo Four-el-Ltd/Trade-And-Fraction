b9b6ddfc665eb30a75f27e1e91d6475df331d9d8
Ruslanllll <innaryslanllll@gmail.com>
2024-03-06 02:12:31 +0200

    shadow?

--- src/main/java/com/warpradar/mixins/minecraft/EntityCreeperMixin.java
+++ src/main/java/com/warpradar/mixins/minecraft/EntityCreeperMixin.java
@@ -5,6 +5,7 @@ import net.minecraft.entity.monster.EntityMob;
 import net.minecraft.world.World;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -15,6 +16,9 @@ public abstract class EntityCreeperMixin extends EntityMob {
     EntityCreeperMixin(World world) {
         super(world);
     }
+
+    @Shadow
+    private World worldObj;
 
     @Inject(at = @At("RETURN"), method = "onDeath")
     protected void onKill(CallbackInfo ci) {
